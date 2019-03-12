990f2bcb3222793375430a1d9fbb0ddb2c6c01ed
daybrayk <braydonajohnson@gmail.com>
2019-03-12 09:42:03 -0400

    Moved some models around, added a pack that may potentially be used for the enemy.

--- Assets/_Scripts/AIScripts/AIController.cs
+++ Assets/_Scripts/AIScripts/AIController.cs
@@ -3,15 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class AIController : MonoBehaviour {
+    public PlayerController pc;
+    public float destinationChangeTimer;
+    private float m_destinationChangeTimer;
     private GameManager gm;
+    private AIFunctionality m_aiFunctionality;
 	// Use this for initialization
 	void Start () {
-		
+        m_destinationChangeTimer = destinationChangeTimer;
+        m_aiFunctionality = GetComponent<AIFunctionality>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        m_destinationChangeTimer -= Time.deltaTime;
+        if(m_destinationChangeTimer <= 0)
+        {
+            if(pc.currentBunker.name == "HouseBunker")
+            {
+                //move toward the door of the house
+            }
+            else
+            {
+                Transform t = pc.currentBunker.playerPosition.transform;
+                m_aiFunctionality.MoveTo(t.position);
+            }
+        }
 	}
 
     private void OnDestroy()
--- Assets/_Scripts/AIScripts/AIFunctionality.cs
+++ Assets/_Scripts/AIScripts/AIFunctionality.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 public class AIFunctionality : MonoBehaviour {
+    public PlayerController pc;
+    [SerializeField] private float attackRange;
     private NavMeshAgent m_aiAgent;
 	// Use this for initialization
 	void Start () {
@@ -11,10 +13,25 @@ public class AIFunctionality : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+        if(Time.frameCount % 5 == 0)
+        {
+            
+        }
 	}
 
-    public void MoveTo()
+    public void MoveTo(Vector3 destination)
+    {
+        m_aiAgent.SetDestination(destination);
+        m_aiAgent.isStopped = false;
+    }
+
+    public void Attack()
+    {
+        m_aiAgent.isStopped = true;
+    }
+
+    public void EnterHouse()
     {
 
     }
